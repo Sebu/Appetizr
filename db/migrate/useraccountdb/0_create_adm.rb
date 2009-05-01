ce2fb1d2d0e1ac077ae2997f7667c3bd5d55d39e
sebu <millennium-x@gmx.net>
2009-05-01 15:16:56 +0200

    fixed migration

---
+++ db/migrate/useraccountdb/0_create_adm.rb
@@ -0,0 +1,26 @@
+
+class CreateAdm < ActiveRecord::Migration
+  def self.up
+    create_table :map, :id => false do |table|
+      table.string :barcode, :limit => 32
+      table.string :account, :limit => 8
+    end
+    create_table :equiv, :primary_key => :alias do |table|
+      table.string :alias, :limit => 32
+      table.string :canonical, :limit => 32
+    end
+    acc = Account.new
+    acc.account = "alg-123"
+    acc.barcode = "1111"
+    acc.save!
+    acc = Account.new
+    acc.account = "co1-200"
+    acc.barcode = "1234"
+    acc.save!
+  end
+
+  def self.down
+    drop_table :map
+    drop_table :equiv
+  end
+end
