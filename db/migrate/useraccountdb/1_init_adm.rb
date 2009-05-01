98c3bb5dfb6cbc3ddf588e5c3f0be14034b2de83
Sebastian Szczepanski <seb@ion2.(none)>
2009-05-01 14:43:26 +0200

    migration fix

--- db/migrate/useraccountdb/1_init_adm.rb
+++ db/migrate/useraccountdb/1_init_adm.rb
@@ -1,14 +1,14 @@
 
 class InitAdm < ActiveRecord::Migration
   def self.up
-    create_table :map, :id => false do |table|
-      table.string :barcode, :limit => 32
-      table.string :account, :limit => 8
-    end
-    create_table :equiv, :primary_key => :alias do |table|
-      table.string :alias, :limit => 32
-      table.string :canonical, :limit => 32
-    end
+    #create_table :map, :id => false do |table|
+    #  table.string :barcode, :limit => 32
+    #  table.string :account, :limit => 8
+    #end
+    #create_table :equiv, :primary_key => :alias do |table|
+    #  table.string :alias, :limit => 32
+    #  table.string :canonical, :limit => 32
+    #end
     acc = Account.new
     acc.account = "alg-123"
     acc.barcode = "1111"
